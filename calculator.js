73c2a05db11b424549786ea494dcf06273841943
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2024-10-06 22:49:55 +0000

    fix errors in calculator.js files

--- calculator.js
+++ calculator.js
@@ -1,35 +1,38 @@
+//functions for the arithmetic
 function add(number1, number2) {
     return number1 + number2;
 }
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+//Event listeners for the arithmetic buttons
 document.getElementById('add').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = add(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
-
-function subtract(number1, number2) {
-    return number1 - number2;
-}
 document.getElementById('subtract').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = subtract(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
-function multiply(number1, number2) {
-    return number1 * number2;
-}
-document.getElementById('multiple').addEventListener('click', function() {
+document.getElementById('multiply').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = multiply(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
-
-function divide(number1, number2) {
-    return number1 / number2;
-}
 document.getElementById('divide').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
